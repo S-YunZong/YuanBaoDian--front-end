02c6e37889cdf2a3d9fbec1a83239a9ddba93d9e
zhihui <1601457588@qq.com>
2020-02-15 18:51:43 +0800

    猿宝典项目

---
+++ src/main/java/com/example/yuanbaodianfrontend/YuanbaodianFrontEndApplication.java
@@ -0,0 +1,22 @@
+package com.example.yuanbaodianfrontend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan("com.example.yuanbaodianfrontend.dao")
+@Configuration
+//定时器
+@EnableScheduling
+@EnableAsync//开启异步注解功能
+public class YuanbaodianFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuanbaodianFrontEndApplication.class, args);
+    }
+
+}
