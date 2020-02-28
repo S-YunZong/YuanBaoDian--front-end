1a986610809bd3087afb50c8ff60d019bf9197cf
tianyu_glb <tianyu_glb@ty.com>
2020-02-28 16:16:30 +0800

    学生喜报

--- src/main/resources/static/js/Training/Training.js
+++ src/main/resources/static/js/Training/Training.js
@@ -10,7 +10,7 @@ $(function () {
                var a="<img src=\"/static/images/Bann01.jpg\" width=\"900px;\">"
                var b="     <h3>学生喜报</h3>"
                for (let i = 0; i <data.length ; i++) {
-                   str=str+" <img src="+data[i].personalPic+" width=\"400\"  style=\"float: left\">\n" +
+                   str=str+" <img src="+data[i].personalPic+" width=\"400\" height='200px' style=\"float: left\">\n" +
                        "                <div >\n" +
                        "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px;\">姓名:"+data[i].userName+"</span> <br><br>\n" +
                        "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px; \" >年龄:"+data[i].age+"</span><br><br>\n" +
