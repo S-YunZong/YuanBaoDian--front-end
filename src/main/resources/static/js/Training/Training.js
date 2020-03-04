a90249aaa19cffcfc2fabc831faea6164fc0caa8
bubataozi <979975516@qq.com>
2020-03-04 18:43:42 +0800

    培训机构-就业喜报

--- src/main/resources/static/js/Training/Training.js
+++ src/main/resources/static/js/Training/Training.js
@@ -7,7 +7,6 @@ $(function () {
        success:function (data) {
            var str="";
            if (data!=null){
-<<<<<<< HEAD
                 str +="<img src=\"/static/images/Bann01.jpg\" width=\"900px;\">"
                str +="     <h3>学生喜报</h3>"
                str +=" <div class=\"container\">";
@@ -26,19 +25,6 @@ $(function () {
                        "                                        <p>薪资待遇："+data[i].salary+"元+"+data[i].treatment+"</p>\n" +
                        "                                    </div>\n" +
                        "                                </div>";
-=======
-               var a="<img src=\"/static/images/Bann01.jpg\" width=\"900px;\">"
-               var b="     <h3>学生喜报</h3>"
-               for (let i = 0; i <data.length ; i++) {
-                   str=str+" <img src="+data[i].personalPic+" width=\"400\" height='200px' style=\"float: left\">\n" +
-                       "                <div >\n" +
-                       "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px;\">姓名:"+data[i].userName+"</span> <br><br>\n" +
-                       "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px; \" >年龄:"+data[i].age+"</span><br><br>\n" +
-                       "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px;\">公司:"+data[i].entrycompany+"</span><br><br>\n" +
-                       "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px;\">薪资："+data[i].salary+"</span><br><br>\n" +
-                       "                    &nbsp; &nbsp; &nbsp; &nbsp;<span style=\"font-size: 20px;\">待遇："+data[i].treatment+"</span><br><br>\n" +
-                       "                </div>"
->>>>>>> origin/master
                }
                str+="</div>";
                str+="</div>";
